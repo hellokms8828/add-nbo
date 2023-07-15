f43ce8125ec6dd22d43ed459f819373c05435f8b
hellokms8828 <hellokms8828@naver.com>
2023-07-15 16:45:24 +0900

    second commit, modify htonl

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <netinet/in.h>
 
 int main(int argc, char **argv) {
     FILE* file1 = fopen(argv[1], "rb");
@@ -19,12 +20,12 @@ int main(int argc, char **argv) {
     size_t fd1 = fread(buf1, sizeof(unsigned char), 4, file1);
     uint32_t* p1 = reinterpret_cast<uint32_t*>(buf1);
     uint32_t n1 = *p1;
-    n1=__builtin_bswap32(n1);
+    n1=htonl(n1);
 
     size_t fd2 = fread(buf2, sizeof(unsigned char), 4, file2);
     uint32_t* p2 = reinterpret_cast<uint32_t*>(buf2);
     uint32_t n2 = *p2;
-    n2=__builtin_bswap32(n2);
+    n2=htonl(n2);
 
 
     printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n",n1, n1, n2, n2, n1+n2, n1+n2);
